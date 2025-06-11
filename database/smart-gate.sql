e9e503a000c21aeb3895e7ef4873d20bd173ede0
Muhammad Dafa Putra <muhammaddavaputra@gmail.com>
2025-06-11 16:20:23 +0700

    Update smart-gate.sql

--- database/smart-gate.sql
+++ database/smart-gate.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 22 Apr 2025 pada 07.42
+-- Waktu pembuatan: 11 Jun 2025 pada 11.20
 -- Versi server: 10.4.28-MariaDB
 -- Versi PHP: 8.2.4
 
@@ -37,6 +37,14 @@ CREATE TABLE `access_logs` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data untuk tabel `access_logs`
+--
+
+INSERT INTO `access_logs` (`log_id`, `tags_id`, `accessed_at`, `status`, `note`, `created_at`, `updated_at`) VALUES
+(1, 2, '2025-04-24 07:04:18', 'Allowed', 'Test', '2025-04-24 00:04:25', '2025-04-24 00:04:25'),
+(2, 1, '2025-06-09 08:16:35', 'Allowed', 'Uji', '2025-06-09 08:16:42', '2025-06-09 08:16:42');
+
 -- --------------------------------------------------------
 
 --
@@ -54,8 +62,12 @@ CREATE TABLE `cache` (
 --
 
 INSERT INTO `cache` (`key`, `value`, `expiration`) VALUES
-('laravel_cache_livewire-rate-limiter:a17961fa74e9275d529f489537f179c05d50c2f3', 'i:1;', 1745300404),
-('laravel_cache_livewire-rate-limiter:a17961fa74e9275d529f489537f179c05d50c2f3:timer', 'i:1745300404;', 1745300404);
+('laravel_cache_livewire-rate-limiter:2981aafe4bb048afdc872e08219ec52ae1d2967e', 'i:1;', 1749460683),
+('laravel_cache_livewire-rate-limiter:2981aafe4bb048afdc872e08219ec52ae1d2967e:timer', 'i:1749460683;', 1749460683),
+('laravel_cache_livewire-rate-limiter:a03482d47b45acecfdd4da52d94a0986080d88b3', 'i:1;', 1749463198),
+('laravel_cache_livewire-rate-limiter:a03482d47b45acecfdd4da52d94a0986080d88b3:timer', 'i:1749463198;', 1749463198),
+('laravel_cache_livewire-rate-limiter:a17961fa74e9275d529f489537f179c05d50c2f3', 'i:1;', 1749463379),
+('laravel_cache_livewire-rate-limiter:a17961fa74e9275d529f489537f179c05d50c2f3:timer', 'i:1749463379;', 1749463379);
 
 -- --------------------------------------------------------
 
@@ -169,7 +181,8 @@ CREATE TABLE `officers` (
 
 INSERT INTO `officers` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
 (1, 'Lord Officer', 'officer@gmail.com', '$2y$12$cC/VnzslQv0SViAGVbvZKedtClAuBzAw5RoM8YxDDH9PDaA4891O6', NULL, '2025-04-18 23:30:16', '2025-04-18 23:30:16'),
-(2, 'Muhammad Dafa Putra', 'dafa@gmail.com', '$2y$12$IjMkXUQtlxbq7SnSh9WGGOaKWbQJGCIndk1ebKq9PslI0ippo2wLu', NULL, '2025-04-18 23:30:30', '2025-04-18 23:30:30');
+(2, 'Muhammad Dafa Putra', 'dafa@gmail.com', '$2y$12$IjMkXUQtlxbq7SnSh9WGGOaKWbQJGCIndk1ebKq9PslI0ippo2wLu', NULL, '2025-04-18 23:30:30', '2025-04-18 23:30:30'),
+(3, 'joni', 'joni@gmail.com', '$2y$12$iTdXUNEYnVUWY7jML8peOeiJsZFG82JL4YFu8L8bP/p91E0yIEqXe', NULL, '2025-06-08 08:30:25', '2025-06-08 08:30:25');
 
 -- --------------------------------------------------------
 
@@ -205,7 +218,8 @@ CREATE TABLE `rfid_tags` (
 
 INSERT INTO `rfid_tags` (`tags_id`, `user_id`, `vehicles_id`, `tag_uid`, `status`, `created_at`, `updated_at`) VALUES
 (1, 1, 2, 'YA2412', 'active', '2025-04-19 20:40:55', '2025-04-19 20:59:32'),
-(2, 2, 1, 'AN12315ZKO', 'active', '2025-04-19 20:48:34', '2025-04-19 21:28:33');
+(2, 2, 1, 'AN12315ZKO', 'inactive', '2025-04-19 20:48:34', '2025-04-24 00:02:50'),
+(3, 9, 4, 'B217ANGGWP', 'active', '2025-06-09 08:13:17', '2025-06-09 08:13:17');
 
 -- --------------------------------------------------------
 
@@ -227,7 +241,8 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('Ivn20dCnqdeULEZULi5SW2JRRnfS1fRBpOkdGExe', 2, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36', 'YTo1OntzOjY6Il90b2tlbiI7czo0MDoiUUtScExkNUxFUE1QTXpQcWJNQjRMcE1jdGYybExIRGRWclgzUVR1SiI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mjc6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9hZG1pbiI7fXM6NTQ6ImxvZ2luX29mZmljZXJfNTliYTM2YWRkYzJiMmY5NDAxNTgwZjAxNGM3ZjU4ZWE0ZTMwOTg5ZCI7aToyO3M6MjE6InBhc3N3b3JkX2hhc2hfb2ZmaWNlciI7czo2MDoiJDJ5JDEyJElqTWtYVVF0bHhicTdTblNoOVdHR09hS1diUUpHQ0luZGsxZWJLcTlQc2xJMGlwcG8yd0x1Ijt9', 1745300345);
+('4ucNf0k5RQT1zY7wd2HAUjRpZ76uBts2dmsZn4NF', 2, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36', 'YTo1OntzOjY6Il90b2tlbiI7czo0MDoicko3cWFLSVBxc2d4WjhXT2FDYmVyN2xqN2w2Y2xrYVRRajFNY2xtTiI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mjc6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9hZG1pbiI7fXM6NTQ6ImxvZ2luX29mZmljZXJfNTliYTM2YWRkYzJiMmY5NDAxNTgwZjAxNGM3ZjU4ZWE0ZTMwOTg5ZCI7aToyO3M6MjE6InBhc3N3b3JkX2hhc2hfb2ZmaWNlciI7czo2MDoiJDJ5JDEyJElqTWtYVVF0bHhicTdTblNoOVdHR09hS1diUUpHQ0luZGsxZWJLcTlQc2xJMGlwcG8yd0x1Ijt9', 1749463356),
+('JfNxsawaMySLDT41HtUvHZgIupRYxYvMgwH3Qcze', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiQXB2cG93ZmFGaVFwaHNIVU9DNHVhSkt4MGhKUE8ySVZ3c0RaOGhMdyI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzM6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9hZG1pbi9sb2dpbiI7fX0=', 1749462994);
 
 -- --------------------------------------------------------
 
@@ -237,7 +252,9 @@ INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, 
 
 CREATE TABLE `users` (
   `user_id` bigint(20) UNSIGNED NOT NULL,
+  `email` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `address` varchar(255) NOT NULL,
   `phone_number` varchar(255) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
@@ -248,11 +265,13 @@ CREATE TABLE `users` (
 -- Dumping data untuk tabel `users`
 --
 
-INSERT INTO `users` (`user_id`, `name`, `address`, `phone_number`, `created_at`, `updated_at`) VALUES
-(1, 'Muhammad Dafa Putra', 'Perum. Muka Kuning Indah 1', '083181531047', '2025-04-18 23:32:08', '2025-04-18 23:32:18'),
-(2, 'Firjatullah Anang Setiawan', 'Batu Aji', '082391512347', '2025-04-18 23:32:30', '2025-04-19 00:06:01'),
-(3, 'Andre Adyatmajaya', 'Batam Center', '085127512745', '2025-04-18 23:32:41', '2025-04-18 23:32:41'),
-(4, 'Yunita Caroline', 'Batu Aji SP', '081329485726', '2025-04-18 23:51:35', '2025-04-21 19:50:07');
+INSERT INTO `users` (`user_id`, `email`, `name`, `password`, `address`, `phone_number`, `created_at`, `updated_at`) VALUES
+(1, '', 'Muhammad Dafa Putra', '', 'Perum. Muka Kuning Indah 1', '083181531047', '2025-04-18 23:32:08', '2025-04-18 23:32:18'),
+(2, '', 'Firjatullah Anang Setiawan', '', 'Batu Aji', '082391512347', '2025-04-18 23:32:30', '2025-04-19 00:06:01'),
+(3, '', 'Andre Adyatmajaya', '', 'Batam Center', '085127512745', '2025-04-18 23:32:41', '2025-04-18 23:32:41'),
+(4, '', 'Yunita Caroline', '', 'Batu Aji SP', '081329485726', '2025-04-18 23:51:35', '2025-04-21 19:50:07'),
+(9, 'ujang12@gmail.com', 'ujang', '$2y$12$4ScWWHqpKdZlfCPAEZN0kemXIeeTOPBI3U1Agn/OI5hWN5wJJtPU2', 'Batu Aji', '083181531047', '2025-06-09 07:25:51', '2025-06-09 07:25:51'),
+(10, 'dhaput@gmail.com', 'Dhaput', '$2y$12$RLA64BhtegL72COOliEQJe8tcijfCwghh89DwvQwca7MsZOjloZPe', 'Busan, Korea', '083181531047', '2025-06-09 09:17:03', '2025-06-09 09:17:03');
 
 -- --------------------------------------------------------
 
@@ -276,8 +295,9 @@ CREATE TABLE `vehicles` (
 --
 
 INSERT INTO `vehicles` (`vehicles_id`, `user_id`, `plate_number`, `vehicle_type`, `brand`, `color`, `created_at`, `updated_at`) VALUES
-(1, 2, 'BA 1231 ZZ', 'Motor', 'Mio', 'Putih', '2025-04-18 23:55:34', '2025-04-19 00:02:12'),
-(2, 1, 'BA 2205 GG', 'Mobil', 'Mazda Miata', 'Merah', '2025-04-18 23:57:56', '2025-04-18 23:57:56');
+(1, 2, 'BA 1231 ZZ', 'Motor', 'Mio Mirza', 'Putih', '2025-04-18 23:55:34', '2025-04-24 00:03:28'),
+(2, 1, 'BA 2205 GG', 'Mobil', 'Mazda Miata', 'Merah', '2025-04-18 23:57:56', '2025-04-18 23:57:56'),
+(4, 9, 'B217AN', 'Mobil', 'Alpard', 'Kuning', '2025-06-09 08:12:54', '2025-06-09 08:12:54');
 
 --
 -- Indexes for dumped tables
@@ -380,7 +400,7 @@ ALTER TABLE `vehicles`
 -- AUTO_INCREMENT untuk tabel `access_logs`
 --
 ALTER TABLE `access_logs`
-  MODIFY `log_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `log_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `failed_jobs`
@@ -404,25 +424,25 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT untuk tabel `officers`
 --
 ALTER TABLE `officers`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT untuk tabel `rfid_tags`
 --
 ALTER TABLE `rfid_tags`
-  MODIFY `tags_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `tags_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `user_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT untuk tabel `vehicles`
 --
 ALTER TABLE `vehicles`
-  MODIFY `vehicles_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `vehicles_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
